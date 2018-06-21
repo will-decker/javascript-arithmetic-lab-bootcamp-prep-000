84e0e3a746bb4b402dfb9e4fd410b328df9f7bc0
William Decker <willdecker@gmail.com>
2018-06-21 06:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  
+  return n++;
 }
 
 function dec(n) {
-  
+  return n--;
 }
 
 function makeInt(n) {
-  
+  return parseInt(n, 10);
 }
 
 function preserveDecimal(n) {
-  
+  return parseFloat(n);
 }
